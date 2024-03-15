d72efff13ac220b5cf243b222f899b2ad20fffc4
wangqi <wangqi@chicv.com>
2024-03-15 11:28:23 +0800

    fix: 运行报错

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-
-
+// @ts-ignore
+import App from './App';
 
 window.addEventListener('resize', () => {
   document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
 })
 document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
     <App />
